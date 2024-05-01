fcd789dc4ce9386ba59b8cd48e91d4263422c90b
Félix-Antoine Gravel <felix.gravel@hotmail.com>
2024-05-01 10:28:44 -0400

    ajout du layout pour le login page

--- app/src/main/java/com/example/tp3veilletechnologique/MainActivity.kt
+++ app/src/main/java/com/example/tp3veilletechnologique/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.tp3veilletechnologique
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,20 +15,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tp3veilletechnologique.ui.theme.TP3VeilleTechnologiqueTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    lateinit var username : EditText
+    lateinit var password : EditText
+    lateinit var passwordCheck : EditText
+    lateinit var connexion : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            TP3VeilleTechnologiqueTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.login_layout)
     }
 }
 
+/*
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -34,10 +37,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+
+
+@Preview(showBackground = false)
 @Composable
 fun GreetingPreview() {
     TP3VeilleTechnologiqueTheme {
-        Greeting("Android")
+        Greeting("papa")
     }
-}
+}*/
